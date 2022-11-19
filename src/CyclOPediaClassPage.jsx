bfb5521a3a0d13072bdf80c972316ae344075835
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-19 13:59:27 -0600

    Saving in LocalStorage

--- src/CyclOPediaClassPage.jsx
+++ src/CyclOPediaClassPage.jsx
@@ -4,7 +4,7 @@ import { getRandomUser } from "./Utility/api";
 class CyclOPediaClassPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = JSON.parse(localStorage.getItem("cylcopediaState")) || {
       instructor: undefined,
       studentList: [],
       studentCount: 0,
@@ -16,21 +16,26 @@ class CyclOPediaClassPage extends React.Component {
 
   componentDidMount = async () => {
     console.log("Component Did Mount");
-    const response = await getRandomUser();
-    console.log(response);
-    this.setState((prevState) => {
-      return {
-        instructor: {
-          name: response.data.first_name + " " + response.data.last_name,
-          email: response.data.email,
-          phone: response.data.phone_number,
-        },
-      };
-    });
+    if (JSON.parse(localStorage.getItem("cylcopediaState"))) {
+      // this.setState(JSON.parse(localStorage.getItem("cylcopediaState")));
+    } else {
+      const response = await getRandomUser();
+      console.log(response);
+      this.setState((prevState) => {
+        return {
+          instructor: {
+            name: response.data.first_name + " " + response.data.last_name,
+            email: response.data.email,
+            phone: response.data.phone_number,
+          },
+        };
+      });
+    }
   };
 
   componentDidUpdate() {
     console.log("Component Did Update");
+    localStorage.setItem("cylcopediaState", JSON.stringify(this.state));
   }
 
   componentWillUnmount() {
