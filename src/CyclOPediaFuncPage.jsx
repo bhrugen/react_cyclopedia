604504f3f6bd754db734b52227e8c8684f75e6ba
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-20 10:33:23 -0600

    UseEffect - Unmount in Action

--- src/CyclOPediaFuncPage.jsx
+++ src/CyclOPediaFuncPage.jsx
@@ -55,37 +55,6 @@ const CyclOPediaFuncPage = () => {
     };
   }, []);
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = JSON.parse(localStorage.getItem("cylcopediaState")) || {
-  //     instructor: undefined,
-  //     studentList: [],
-  //     studentCount: 0,
-  //     hideInstructor: false,
-  //     inputName: "",
-  //     inputFeedback: "",
-  //   };
-  // }
-
-  // componentDidMount = async () => {
-  //   console.log("Component Did Mount");
-  //   if (JSON.parse(localStorage.getItem("cylcopediaState"))) {
-  //     // this.setState(JSON.parse(localStorage.getItem("cylcopediaState")));
-  //   } else {
-  //     const response = await getRandomUser();
-  //     console.log(response);
-  //     this.setState((prevState) => {
-  //       return {
-  //         instructor: {
-  //           name: response.data.first_name + " " + response.data.last_name,
-  //           email: response.data.email,
-  //           phone: response.data.phone_number,
-  //         },
-  //       };
-  //     });
-  //   }
-  // };
-
   // componentDidUpdate = async (previousProps, previousState) => {
   //   console.log("Component Did Update");
   //   localStorage.setItem("cylcopediaState", JSON.stringify(this.state));
